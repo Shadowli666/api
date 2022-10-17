3dc19979e3b964b7be717e447a7106d2a18c7344
shadowli666 <shadowlf666@gmail.com>
2022-10-16 22:31:38 -0400

    cambio de source

---
+++ README.md
@@ -0,0 +1,8 @@
+# MODELO PARA IMPRIMIR EL PRECIO DEL DÓLAR
+Hecho con Boostrap, Jquery y <3
+
+Gracias a **Ivan Montilla** por facilitar la API.
+
+**¡Sólo presiona Ctrl + P e imprime!**
+
+*igual espero aprender webscraping algún día*
--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+
+// DOLAR TODAY
 //https://s3.amazonaws.com/dolartoday/data.json
-$.getJSON("https://s3.amazonaws.com/dolartoday/data.json",function(data){
-  console.log(data)
-  $('#texto').html('BCV: ' + data.USD.sicad2);
-  $('#al').html(data._timestamp.fecha);
-    });
+// BCV
+//https://api.exchangedyn.com/markets/quotes/usdves/bcv?fbclid=IwAR0VKRtIIlO3tthfNKJ2tu9EUmtujb-6VuxfwfrKgMbaY1fAE8et8w1jQuM
+// $.getJSON("https://s3.amazonaws.com/dolartoday/data.json",function(data){
+//   console.log(data)
+//   $('#texto').html('BCV: ' + data.USD.sicad2);
+//   $('#al').html(data._timestamp.fecha);
+//     });
+
+$.getJSON("https://api.exchangedyn.com/markets/quotes/usdves/bcv?fbclid=IwAR0VKRtIIlO3tthfNKJ2tu9EUmtujb-6VuxfwfrKgMbaY1fAE8et8w1jQuM",function(data){
+      const navigatorLanguage = navigator.language
+      //bank source name
+      $('#banco').html(data.name)
+      //price 
+      $('#texto').html(data.sources.BCV.quote)
+      //date retrieved
+      let date = new Date(data.sources.BCV.last_retrieved)
+      let dateOptions = {year: 'numeric', month: 'long', day: 'numeric'};
+      let hourOptions = {hour: '2-digit', minute: '2-digit'};
+      formatedDate = date.toLocaleDateString(navigatorLanguage,dateOptions)
+      //formatedHour = date.toLocaleDateString([],{})
+      $('#al').html(formatedDate)
+      //$('#el').html(formatedHour)
+        });
